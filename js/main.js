d585ba991b91b5c0e39e4147be011a9a9d8da70e
Abril-cantera <abril.gonzalez.cantera@gmail.com>
2023-01-27 17:42:02 -0300

    Cambiando estilos

--- js/main.js
+++ js/main.js
@@ -7,23 +7,30 @@ async function getSelection() {
   const response = await fetch("http://localhost:5000/api/v1/selection");
   let data = await response.json();
 
+  const clickSelection = document.getElementById('clickSelection')
+  clickSelection.addEventListener('click', openContainerSelection)
+  function openContainerSelection() {
+    console.log('funciona')
+  }
+
   for (let i = 0; i < data.length; i++) {
     const containerPrincipal = document.getElementById('containerPrincipal');
     containerPrincipal.classList.toggle('inactive');
-    const div = document.createElement("div");
-    const section = document.createElement("section");
+    const divSelection = document.createElement("div");
+    const sectionSelection = document.createElement("section");
+    sectionSelection.classList.add('sectionSelection')
     let h2 = document.createElement("h2");
     let img = document.createElement("img");
     img.classList.add('imgSelection');
 
-
     h2 = data[i].nameEquipment;
     img.src = data[i].image;
 
-    section.append(h2);
-    section.append(img);
-    div.appendChild(section);
-    containerPrincipal.append(div);
+
+    sectionSelection.append(h2);
+    sectionSelection.append(img);
+    divSelection.appendChild(sectionSelection);
+    containerPrincipal.append(divSelection);
 
   }
   console.log(data);

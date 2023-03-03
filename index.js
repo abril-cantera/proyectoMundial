418a5dc57f8988da7c8cfebd5b14ebf037164944
Abril-cantera <abril.gonzalez.cantera@gmail.com>
2023-03-03 17:33:54 -0300

    Cambios de database con AWS, seguir en casa

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-const whitelist = ['http://localhost:8080', 'https://myapp.co', 'http://localhost:5500', 'http://localhost:3001', 'http://localhost:5001'];
+const whitelist = ['http://localhost:8080', 'https://myapp.co', 'http://localhost:5500', 'http://localhost:3001', 'http://13.59.141.189:5000'];
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin) || !origin) {

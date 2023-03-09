b36a625710cf64a6c18d5952592f91d7d58f391f
Abril-cantera <abril.gonzalez.cantera@gmail.com>
2023-03-09 11:52:17 -0300

    ..

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-const whitelist = ['http://localhost:8080', 'https://myapp.co', 'http://localhost:5500', 'http://localhost:3001', 'http://ec2-3-15-27-134.us-east-2.compute.amazonaws.com/5000'];
+const whitelist = ['http://localhost:8080', 'https://myapp.co', 'http://localhost:5500', 'http://localhost:3001', '18.188.94.82/5000'];
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin) || !origin) {
@@ -35,3 +35,5 @@ app.get('/', (req, res) => res.send('Hello Word'))
 app.listen(port, () => {
   console.log(`Mi port ${port}`);
 });
+
+
---
+++ src/components/GetGroup/index.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react"
+import '../styles.css'
+
+
+export function GetGroup() {
+
+  const [group, setGroup] = useState([])
+  const url = "http://18.188.94.82:5000/api/v1/group"
+
+  const fetchApiGroup = async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    setGroup(data)
+  }
+
+  useEffect(() => {
+    fetchApiGroup()
+  }, [])
+
+  return (
+    <div className="containerPricipal" id="containerGroup">
+      <h2 className="text">Face de Grupo</h2>
+      <div className="containerCard">
+        {
+          group.map((item) =>
+            <div className="card" key={item.id}>
+              <h2 className="text2">{item.nameGroup}</h2>
+              <section>
+                {item.selection.map(item =>
+                  <section key={item.id}>
+                    <h4 className="text3">{item.nameEquipment}</h4>
+                  </section>
+                )}
+              </section>
+            </div>)
+        }
+      </div>
+
+    </div>
+  );
+}

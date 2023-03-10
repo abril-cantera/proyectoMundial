ba9fe43382b3581e5b18114347e1d04b02f8622e
Abril-cantera <abril.gonzalez.cantera@gmail.com>
2023-03-10 17:33:58 -0300

    Elimine src

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-const whitelist = ['http://localhost:8080', 'https://myapp.co', 'http://localhost:5500', 'http://3.144.244.212:5000/api/v1/', 'http://3.144.244.212:5000/api/v1/group', 'http://3.144.244.212:5000/api/v1/selection', 'http://3.144.244.212:5000/api/v1/player', 'http://3.144.244.212:5000/api/v1/match'];
+const whitelist = ['http://localhost:8080', 'https://myapp.co', 'http://localhost:3000', 'http://3.144.244.212:5000/api/v1/', 'http://3.144.244.212:5000/api/v1/group', 'http://3.144.244.212:5000/api/v1/selection', 'http://3.144.244.212:5000/api/v1/player', 'http://3.144.244.212:5000/api/v1/match'];
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin) || !origin) {

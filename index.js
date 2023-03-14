1355883919345e506b6f7526ab69d6c776d33c6b
Abril-cantera <abril.gonzalez.cantera@gmail.com>
2023-03-14 10:55:19 -0300

    ..

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-const whitelist = ['http://localhost:8080', 'https://myapp.co', 'http://localhost:3000', 'http://3.144.244.212:5000/api/v1/', 'http://3.144.244.212:5000/api/v1/group', 'http://3.144.244.212:5000/api/v1/selection', 'http://3.144.244.212:5000/api/v1/player', 'http://3.144.244.212:5000/api/v1/match'];
+const whitelist = ['http://localhost:8080', 'https://myapp.co', 'http://localhost:3000', 'http://52.15.224.86:5000/api/v1/', 'http://52.15.224.86:5000/api/v1/group', 'http://52.15.224.86:5000/api/v1/selection', 'http://52.15.224.86:5000/api/v1/player', 'http://52.15.224.86:5000/api/v1/match'];
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin) || !origin) {

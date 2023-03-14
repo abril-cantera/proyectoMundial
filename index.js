3a64c9c26cbd0a8e277b3430928e4740311cd55c
Abril-cantera <abril.gonzalez.cantera@gmail.com>
2023-03-14 11:40:16 -0300

    ..

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-const whitelist = ['http://localhost:8080', 'https://myapp.co', 'http://192.168.100.48:3000', 'http://localhost:3000', 'http://52.15.224.86:5000/api/v1/', 'http://52.15.224.86:5000/api/v1/group', 'http://52.15.224.86:5000/api/v1/selection', 'http://52.15.224.86:5000/api/v1/player', 'http://52.15.224.86:5000/api/v1/match', 'https://app-mundial-frontend.vercel.app/#'];
+const whitelist = ['https://localhost:8080', 'httpss://myapp.co', 'https://192.168.100.48:3000', 'https://localhost:3000', 'https://52.15.224.86:5000/api/v1/', 'https://52.15.224.86:5000/api/v1/group', 'https://52.15.224.86:5000/api/v1/selection', 'https://52.15.224.86:5000/api/v1/player', 'https://52.15.224.86:5000/api/v1/match', 'https://app-mundial-frontend.vercel.app/#'];
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin) || !origin) {
